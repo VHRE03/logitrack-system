6e5cf8685436ccfc2d1ef1d493d64e4b976028a9
VHRE03 <vh.romero.e@gmail.com>
2026-07-27 16:28:52 -0600

    feature: Integracionn de discovery-service y de warehouse-service

---
+++ discovery-service/src/test/java/com/vhre/discovery_service/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.vhre.discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+@EnableEurekaServer
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
