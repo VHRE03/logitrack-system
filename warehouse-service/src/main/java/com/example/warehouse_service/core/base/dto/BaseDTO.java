c223257f6d38fe89fda95e14415393003ff86120
VHRE03 <vh.romero.e@gmail.com>
2026-07-28 16:32:09 -0600

    feature(warehouse-service): Integracion de una entidad base

---
+++ warehouse-service/src/main/java/com/example/warehouse_service/core/base/dto/BaseDTO.java
@@ -0,0 +1,32 @@
+package com.example.warehouse_service.core.base.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Null;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record BaseDTO(
+        @Schema(
+                description = "Unique identifier of the resource (UUID). Automatically generated.",
+                example = "123e4567-e89b-12d3-a456-426614174000",
+                accessMode = Schema.AccessMode.READ_ONLY
+        )
+        @Null(message = "The ID must be null when creating a new resource")
+        UUID id,
+
+        @Schema(
+                description = "Timestamp indicating when the resource was created.",
+                example = "2023-10-25T10:00:00",
+                accessMode = Schema.AccessMode.READ_ONLY
+        )
+        LocalDateTime createdAt,
+
+        @Schema(
+                description = "Timestamp indicating the last time the resource was updated.",
+                example = "2023-10-25T14:30:00",
+                accessMode = Schema.AccessMode.READ_ONLY
+        )
+        LocalDateTime updatedAt
+) {
+}
